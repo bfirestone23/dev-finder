bbd9540e9f1f99c0df12d95b1ac5c35e66429860
Brian Firestone <bfirestone2339@gmail.com>
2021-03-04 17:31:37 -0500

    Add route for Omniauth callbacks and registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :jobs
 
   root 'application#home'
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
