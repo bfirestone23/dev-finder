35e99cb99bf6e2a30616da1ce6dfb2ab749d196f
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 13:47:21 -0500

    Remove new_location_params, implement job creation from nested resource

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,24 +3,42 @@ class JobsController < ApplicationController
     before_action :redirect_non_employers, only: [:new, :create, :edit, :update, :destroy]
 
     def new
-        @job = Job.new  
-        @locations = Location.all
+        if params[:location_id]
+            @location = Location.find_by(id: params[:location_id])
+            @job = @location.jobs.new 
+            render :new_from_location
+        else
+            @job = Job.new  
+            @locations = Location.all
+        end
     end
 
     def create 
-        @job = Job.new(jobs_params)
-        @job.users << current_user
-        
-        if !new_location_params[:city].empty?
-            location = Location.find_or_create(new_location_params)
-            @job.location = location
-        end
+        if params[:location_id]
+            @location = Location.find_by(id: params[:location_id])
+            @job = @location.jobs.new(jobs_params)
+            @job.users << current_user
 
-        if @job.save
-            redirect_to location_job_path(@job.location, @job)
+            if @job.save
+                redirect_to location_job_path(@job.location, @job)
+            else
+                render :new_from_location
+            end
         else
-            @locations = Location.all
-            render :new
+            @job = Job.new(jobs_params)
+            @job.users << current_user
+            
+            if !params[:job][:city].empty?
+                location = Location.find_or_create_by(city: params[:job][:city])
+                @job.location = location
+            end
+
+            if @job.save
+                redirect_to location_job_path(@job.location, @job)
+            else
+                @locations = Location.all
+                render :new
+            end
         end
     end
 
@@ -52,10 +70,6 @@ class JobsController < ApplicationController
 
     def jobs_params
         params.require(:job).permit(:title, :description, :pay, :location_id)
-    end
-
-    def new_location_params
-        params.require(:job).permit(:city)
     end
 
     def redirect_non_employers
