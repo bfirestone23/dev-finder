fe9eb9957a718b44fc93222d05fb81292961102f
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 11:03:16 -0500

    Update #facebook to include invalid user handling

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+        
+        if @user.valid?
+            sign_in_and_redirect @user
+        else
+            render 'devise/sessions/new'
+        end
     end
 end
