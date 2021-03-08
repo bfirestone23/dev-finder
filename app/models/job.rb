48838005b9b213dd918ab5e637b8830a8c650f05
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 13:47:53 -0500

    Remove unnecessary #accepts_nested_attributes_for methods

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,7 +2,4 @@ class Job < ApplicationRecord
     belongs_to :location
     has_many :users_jobs
     has_many :users, through: :users_jobs
-
-    accepts_nested_attributes_for :users 
-    accepts_nested_attributes_for :location
 end
