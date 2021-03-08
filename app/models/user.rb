ea62d61ec9b2619cd1bfe2f34a68767609495b4f
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 11:04:37 -0500

    Remove comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,8 @@ class User < ApplicationRecord
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
     user.email = auth.info.email
     user.password = Devise.friendly_token[0, 20]
-    user.name = auth.info.name   # assuming the user model has a name
-    user.image = auth.info.image # assuming the user model has an image
-
+    user.name = auth.info.name   
+    user.image = auth.info.image 
     user.save
     user
   end
