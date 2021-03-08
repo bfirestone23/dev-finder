a0cf37c5fa8442742559e1154adf373b222509c3
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 11:10:56 -0500

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Location.create(city: "New York City")
+Location.create(city: "Los Angeles")
+Location.create(city: "Chicago")
