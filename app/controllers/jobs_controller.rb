3d22ceb3c38f22fcd786eb8d8d30aa0f2bf8e2d4
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 11:02:29 -0500

    Create #new_location_params and update #create action to handle user-provided location, update #index tlogic to account for no location being found

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -9,25 +9,34 @@ class JobsController < ApplicationController
 
     def create 
         @job = Job.new(jobs_params)
+        @job.users << current_user
+        
+        if !new_location_params[:city].empty?
+            location = Location.find_or_create(new_location_params)
+            @job.location = location
+        end
 
         if @job.save
             redirect_to location_job_path(@job.location, @job)
         else
+            @locations = Location.all
             render :new
         end
     end
 
     def index
         if params[:location_id]
-            @location = Location.find_by(params[:location_id])
-            @jobs = @location.jobs
+            @location = Location.find_by(id: params[:location_id])
+            if @location
+                @jobs = @location.jobs
+            end
         else
             @jobs = Job.all
         end
     end
 
     def show
-        @job = Job.find(params[:id])
+        @job = Job.find_by(id: params[:id])
     end
 
     def edit
@@ -43,6 +52,10 @@ class JobsController < ApplicationController
 
     def jobs_params
         params.require(:job).permit(:title, :description, :pay, :location_id)
+    end
+
+    def new_location_params
+        params.require(:job).permit(:city)
     end
 
     def redirect_non_employers
