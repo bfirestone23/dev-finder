245725542e1732e6d4ae6a91375e06b23793b90b
Brian Firestone <bfirestone2339@gmail.com>
2021-03-08 11:04:13 -0500

    Add #accepts_nested_attributes_for users & location

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,7 @@ class Job < ApplicationRecord
     belongs_to :location
     has_many :users_jobs
     has_many :users, through: :users_jobs
+
+    accepts_nested_attributes_for :users 
+    accepts_nested_attributes_for :location
 end
