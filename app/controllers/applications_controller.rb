b5fb388115e2a300a3b54b625b2e2cdf5af9f38c
Brian Firestone <bfirestone2339@gmail.com>
2021-03-09 12:30:42 -0500

    Implement #new, #create, and #application_params

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationsController < ApplicationController
     def new 
+        @job = Job.find_by(id: params[:job_id])
+        @application = @job.applications.build
     end
 
     def create 
+        job = Job.find_by(id: params[:job_id])
+        job.users << current_user
+        @application = job.applications.new(application_params)
+        
+        if @application.save
+            redirect_to job_application_path(job, @application)
+        else
+            render :new
+        end
     end
 
     def index 
@@ -18,5 +29,17 @@ class ApplicationsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def application_params
+        params.require(:application).permit(:previous_job, 
+            :previous_experience, 
+            :years_experience, 
+            :cover_letter, 
+            :date_available,
+            :user_id
+        )
     end
 end
