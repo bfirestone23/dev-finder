e0f1f60bc6dbd6c8f1256d94029fbc8be3c3cdea
Brian Firestone <bfirestone2339@gmail.com>
2021-03-09 11:21:28 -0500

    Create Application model and establish associations

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,5 @@ class Job < ApplicationRecord
     belongs_to :location
     has_many :users_jobs
     has_many :users, through: :users_jobs
+    has_many :applications
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :users_jobs
   has_many :jobs, through: :users_jobs
+  has_many :applications 
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
