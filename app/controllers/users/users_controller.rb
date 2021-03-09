7789b310471d597e7338f9a501c25ec8b5f6ea2c
Brian Firestone <bfirestone2339@gmail.com>
2021-03-09 13:31:07 -0500

    Add logic in #show to render employer or applicant show page

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,6 +2,12 @@ class Users::UsersController < ApplicationController
     before_action :authenticate_user!
 
     def show
-        render 'devise/users/show'
+        if current_user.employer 
+            @jobs = current_user.jobs
+            render 'devise/users/show_employer'
+        else
+            @applications = current_user.applications
+            render 'devise/users/show_applicant'
+        end
     end
 end
