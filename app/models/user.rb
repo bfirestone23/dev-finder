ea7d176b1ce1db884788bca05b9132a2ec4af056
Brian Firestone <bfirestone2339@gmail.com>
2021-03-10 12:00:31 -0500

    Establish has many through association between users, locations, and applicants

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :jobs
   has_many :applications 
+  has_many :locations, through: :applications
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
