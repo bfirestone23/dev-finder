cb1df55c6c1e1e16c45a5d664cf03e6a171b5342
Brian Firestone <bfirestone2339@gmail.com>
2021-03-11 12:51:07 -0500

    Update show action to render views dynamically based on user_path and current_user, redirect if non-employer attempting to view other non-employer user page

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -2,12 +2,23 @@ class Users::UsersController < ApplicationController
     before_action :authenticate_user!
 
     def show
-        if current_user.employer 
-            @jobs = Job.where(user_id: current_user.id)
+        @user = User.find_by(id: params[:id])
+
+        if @user.employer 
+            @jobs = Job.where(user_id: @user.id)
             render 'devise/users/show_employer'
         else
-            @applications = current_user.applications
-            render 'devise/users/show_applicant'
+            if @user == current_user
+                @applications = current_user.applications
+                render 'devise/users/show_applicant'
+            else
+                redirect_to root_path, notice: "You do not have access to that page."
+            end
         end
+    end
+
+    def index
+        @users = User.where(employer: true)
+        render 'devise/users/index'
     end
 end
