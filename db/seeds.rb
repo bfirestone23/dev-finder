c9ff5293f4c5d12c6fb58696366d937bfb17910b
Brian Firestone <bfirestone2339@gmail.com>
2021-03-10 20:42:15 -0500

    Add Job data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Location.create(city: "New York City")
-Location.create(city: "Los Angeles")
-Location.create(city: "Chicago")
+# Location.create(city: "New York City")
+# Location.create(city: "Los Angeles")
+# Location.create(city: "Chicago")
 
+
+Job.create(
+    title: "DevOps Engineer",
+    description: 
+        "Team and Role Overview
+
+        We are looking for a DevOps Engineer to join our growing team at our headquarters.  In this role, you will be part of a team supporting our company’s efforts in expanding our DevOps practices across our printing and rocket manufacturing teams.  You will be responsible for the day to day support of our DevOps environments, applications, and processes; working closely with development and IT teams to implement best practices and automation.
+        
+        The Mission/Outcomes and Objectives
+        
+        To help us support our growing 3D printing capabilities and rocket manufacturing, as a DevOps Engineer at Relativity, you will
+        
+        Enhance Relativity’s DevOps team to support our rapid growth across multiple sites.
+        Ensure continued up-time on our services, processes, and infrastructure throughout the company and across all of our sites.
+        Actively participate in the team’s daily tasking and support requirements.
+        Assist the DevOps team in our roadmap for automated software deployments across the company.
+        Ensure we continue to implement and follow best practices across our systems and infrastructure to achieve our mission and goals.
+        Candidate Profile
+        
+        To be successful in this role, you should have 5+ years of experience as a DevOps Engineer, ideally in a small-to-midsized, high-growth environments at a technology company. Prior experience in managing cloud environments, containerized workflows, CI/CD pipelines, and a deep passion for hands-on technical problem solving are critical. You thrive in ambiguous environments, are passionate about technology and finding ways to automate tasks, and have an untiring focus on excellent results.
+        
+        Minimum Required Skills and Competencies
+        
+        Bachelor’s degree in a technical field to include one of the following (computer science, computer information systems, engineering, enterprise systems administration, etc.) and 5+ years of experience in system administration.
+        Expert knowledge working in cloud technologies.
+        Expert knowledge of container development and deployments.
+        Expert knowledge of managing CI/CD pipelines.
+        Expert knowledge in managing Atlassian products including Jira, Confluence, BitBucket, and Bamboo.
+        Familiarity with Agile Development processes.
+        Scripting experience in Powershell, Python, or Perl.",
+    
+    location_id: 1,
+    user_id: 1
+)
