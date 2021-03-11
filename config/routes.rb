ddddf9c2ebf145420fbed5446b8e09334d397e87
Brian Firestone <bfirestone2339@gmail.com>
2021-03-11 12:59:19 -0500

    Add users#index route

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
   get '/users/:id', to: 'users/users#show', as: 'user'
+  get '/users', to: 'users/users#index', as: 'users'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
