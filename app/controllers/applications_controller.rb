df9789574d56c5fde473f3a818e33ad15dcf3cf4
Brian Firestone <bfirestone2339@gmail.com>
2021-03-12 10:50:26 -0500

    Add before actions, #set_job, #find_application, #redirect_employers helpers and refactor all actions

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,37 +1,30 @@
 class ApplicationsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :set_job, only: [:new, :create, :index, :update]
+    before_action :find_application, only: [:show, :update]
+
     def new 
-        @job = Job.find_by(id: params[:job_id])
         @application = @job.applications.build
     end
 
     def create 
-        job = Job.find_by(id: params[:job_id])
-        
-        @application = job.applications.new(application_params)
+        @application = @job.applications.new(application_params)
         
         if @application.save
-            redirect_to job_application_path(job, @application)
+            redirect_to job_application_path(@job, @application)
         else
             render :new
         end
     end
 
     def show
-        @application = Application.find_by(id: params[:id])
     end
 
     def index
-        @job = Job.find_by(id: params[:job_id])
-        @applications = @job.applications
-    end
-
-    def edit
+        @applications = @job.applications.order(created_at: :desc)
     end
     
     def update 
-        @application = Application.find_by(id: params[:id])
-        @job = Job.find_by(id: params[:job_id])
-
         @application.update(status: application_params[:status])
         redirect_to job_applications_path(@job)
     end
@@ -47,5 +40,19 @@ class ApplicationsController < ApplicationController
             :status,
             :user_id
         )
+    end
+
+    def set_job
+        @job = Job.find_by(id: params[:job_id])
+    end
+
+    def find_application
+        @application = Application.find_by(id: params[:id])
+    end
+
+    def redirect_employers
+        if current_user.employer
+            redirect_to root_path, notice: "You do not have access to that page."
+        end
     end
 end
