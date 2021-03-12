1b16c78d918362535d40bbb6e8fc4652ec887cac
Brian Firestone <bfirestone2339@gmail.com>
2021-03-12 12:06:02 -0500

    Add #created_date and #welcome_text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+    def created_date(object)
+        object.created_at.strftime("%m/%d/%Y")
+    end
+
+    def welcome_text
+        if !current_user
+            render "no_user_welcome"
+        elsif current_user.employer
+            render "employer_welcome"
+        else
+            render "applicant_welcome"
+        end
+    end
 end
