277b2e840a0e45a15dc7114d2825bb0f8d7fc5f0
Brian Firestone <bfirestone2339@gmail.com>
2021-03-12 15:16:25 -0500

    Add #nav_links, #flash_errors, and #user_detail helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,24 @@ module ApplicationHelper
             render "applicant_welcome"
         end
     end
+
+    def nav_links
+        if !current_user
+            render "logged_out_nav"
+        else
+            render "logged_in_nav"
+        end
+    end
+
+    def flash_errors
+        if flash[:notice]
+            render "flash"
+        end
+    end
+
+    def user_detail
+        if current_user
+            render "user_details"
+        end
+    end
 end
