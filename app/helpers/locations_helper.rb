7b12209bf538e717011fa14794e064ad8b61b123
Brian Firestone <bfirestone2339@gmail.com>
2021-03-12 13:06:00 -0500

    Create #job_company_count and #open_positions

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -1,2 +1,16 @@
 module LocationsHelper
+
+    def job_company_count(location)
+        if location.jobs.empty?
+            render "locations/no_results"
+        else
+            render partial: "locations/job_company_count", locals: { location: location }
+        end
+    end
+
+    def open_positions(location)
+        if location.jobs.count > 0
+            render partial: "locations/open_positions", locals: { location: location }
+        end
+    end
 end
