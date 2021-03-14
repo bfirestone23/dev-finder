30662008b5f15784fb02b0c4cb9c77d8fa7536c5
Brian Firestone <bfirestone2339@gmail.com>
2021-03-14 15:39:36 -0400

    Add #view_jobs_in_button

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -2,9 +2,17 @@ module LocationsHelper
 
     def job_company_count(location)
         if location.jobs.empty?
-            render "locations/no_results"
+            content_tag :p, class: "card-title" do
+                "No jobs posted for this location."
+            end
         else
             render partial: "locations/job_company_count", locals: { location: location }
+        end
+    end
+
+    def view_jobs_in_button(location)
+        unless location.jobs.empty?
+            button_to "View Jobs in #{location.city}", location_jobs_path(location), class: "btn btn-outline-primary float-end", method: :get
         end
     end
 
