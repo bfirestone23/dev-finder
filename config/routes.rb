96d8cf814307960ab0013e0c879f23d14577c372
Brian Firestone <bfirestone2339@gmail.com>
2021-03-14 17:29:31 -0400

    Create /confirm route + view to confirm user type

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,15 @@ Rails.application.routes.draw do
 
   root 'application#home'
 
+  devise_scope :user do 
+    get '/confirm', to: 'users/omniauth_callbacks#landing', as: 'landing'
+    post '/confirm/:params', to: 'users/omniauth_callbacks#facebook'
+  end
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
   get '/users/:id', to: 'users/users#show', as: 'user'
   get '/users', to: 'users/users#index', as: 'users'
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
