51b18eed9e30f57b9075e362702d4e5e90fa0023
Brian Firestone <bfirestone2339@gmail.com>
2021-03-14 13:08:23 -0400

    Create #employer_controls

--- app/helpers/applications_helper.rb
+++ app/helpers/applications_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationsHelper
 
-
+    def employer_controls(application)
+        if application.job.user == current_user
+            if application.status == "Pending"
+                render "applications/employer_controls", application: application
+            else
+                "Your response has been submitted to the applicant!"
+            end
+        end
+    end
 
 end
