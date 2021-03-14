0ac2112e10f6da76e69c383c045e00995578568a
Brian Firestone <bfirestone2339@gmail.com>
2021-03-14 11:17:35 -0400

    Update name of #open_positions and create #company_companies

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -8,9 +8,14 @@ module LocationsHelper
         end
     end
 
-    def open_positions(location)
+    def open_positions_by_location(location)
         if location.jobs.count > 0
             render partial: "locations/open_positions", locals: { location: location }
         end
     end
+
+    def company_companies(location)
+        location.users.uniq.count > 1 ? "companies" : "company"
+    end
+
 end
