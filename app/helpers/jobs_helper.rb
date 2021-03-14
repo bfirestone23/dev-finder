da847b5fe20d9bfb118f9288c5ee9c090f05d284
Brian Firestone <bfirestone2339@gmail.com>
2021-03-14 11:22:53 -0400

    Create #job_titles_by and remove #user_controls

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,16 +1,13 @@
 module JobsHelper
 
-    def user_controls(job)
-        if current_user == job.user 
-            render partial: "jobs/controls/employer_controls", locals: { job: job }
-        else
-            render partial: "jobs/controls/applicant_controls", locals: { job: job }
-        end
-    end
-
     def post_job_button(location=nil)
         if current_user.employer && location
              render partial: "jobs/controls/post_job_by_location", locals: { location: location }
         end
     end
+
+    def job_titles_by(object)
+        object.jobs.map {|j| j.title }.join(", ")
+    end
+
 end
