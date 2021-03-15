a969c29a10a5906627ec22dff0ba2a1d37088fb5
Brian Firestone <bfirestone2339@gmail.com>
2021-03-15 14:36:38 -0400

    Create #add_to_session and #staging actions, implement session[:employer] in #facebook action

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-    def facebook        
-        @user = User.from_omniauth(request.env["omniauth.auth"])
+    def facebook
+        employer = false
+        if session[:employer] == 1
+            employer = true
+        end
+
+        @user = User.from_omniauth(request.env["omniauth.auth"], employer)
         
         if @user.valid?
             sign_in_and_redirect @user
@@ -13,4 +18,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def landing
         render 'devise/registrations/landing'
     end
+
+    def add_to_session
+        if params[:employer] == "1"
+            session[:employer] = 1
+        else
+            session[:employer] = 0
+        end
+        redirect_to staging_path
+    end
+
+    def staging
+        render 'devise/registrations/facebook'
+    end
+
 end
