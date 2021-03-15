58214fa9d0dbb0f48513fb832145ba227947a63d
Brian Firestone <bfirestone2339@gmail.com>
2021-03-15 14:39:36 -0400

    Update .from_omniauth to accept employer parameter, update user.employer

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,12 @@ class User < ApplicationRecord
 
   scope :employers, -> { where(employer: true) }
 
-  def self.from_omniauth(auth)
+  def self.from_omniauth(auth, employer)
     user = find_or_initialize_by(provider: auth.provider, uid: auth.uid)
     user.email = auth.info.email
     user.password = Devise.friendly_token[0, 20]
-    user.name = auth.info.name   
+    user.name = auth.info.name
+    user.employer ||= employer
     user.image = auth.info.image 
     user.save
     user
