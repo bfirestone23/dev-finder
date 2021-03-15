ae36a302734f0f450ddfe6af895fa2644c966bd3
Brian Firestone <bfirestone2339@gmail.com>
2021-03-15 14:35:15 -0400

    Set #after_sign_in_path_for as root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
     private
     
     def after_sign_in_path_for(resource)
-        request.env['omniauth.origin'] || root_path
+        root_path
     end
 end
